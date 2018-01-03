04c2b95dfa654ae3fd9ad5e124b7b7c88675b4e1
Elena <elenacerezo83@gmail.com>
2018-01-03 12:36:32 +0100

    add new white-boxes branch

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// colapsables
+var whiteHeadings = document.querySelector('.panel-heading');
+
+function toggleInput(target){
+  if (target.classList.contains('hidden'))
+  target.classList.remove('hidden');
+  // aside.classList.remove('hidden');
+  else
+  target.classList.add('hidden');
+  // aside.classList.add('hidden');
+ }
+whiteHeadings.addEventListener('click', toggleInput);
