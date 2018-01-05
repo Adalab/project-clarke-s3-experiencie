dd8f57578951cfcb539cfbb51ee14bc17e5e8dc9
Elena <elenacerezo83@gmail.com>
2018-01-05 10:15:59 +0100

    bootstrap grid to white-boxes component

--- js/main.js
+++ js/main.js
@@ -1,14 +1,57 @@
 'use strict';
 
 // colapsables
-var whiteHeadings = document.querySelector('.panel-heading');
+var showStories = document.querySelectorAll('.flecha-abajo');
+var stories = document.querySelector('.panel-body');
+
+// function toggleInput(){
+//   if (showStories.classList.contains('hidden')){
+// showStories.classList.remove('hidden');
+// stories.classList.remove('hidden');
+// }
+//   else {
+//   showStories.classList.add('hidden');
+//   stories.classList.add('hidden');
+//    }
+// }
 
 function toggleInput(target){
-  if (target.classList.contains('hidden'))
-  target.classList.remove('hidden');
-  // aside.classList.remove('hidden');
-  else
+  if (target.classList.contains('hidden')){
+target.classList.remove('hidden');
+// target.classList.remove('hidden');
+}
+  else {
   target.classList.add('hidden');
-  // aside.classList.add('hidden');
- }
-whiteHeadings.addEventListener('click', toggleInput);
+  // target.classList.add('hidden');
+   }
+}
+
+var toggle = function(){
+  document.querySelectorAll('.flecha-abajo').forEach(toggleInput);
+  var stories = document.querySelector('.panel-body');
+  toggleInput(stories);
+  };
+  document.querySelectorAll('.flecha-abajo').forEach(function (input) {
+    input.addEventListener('click', toggle);
+  });
+
+
+
+
+showStories.addEventListener('click', toggleInput(input));
+
+
+// var toggle = function(){
+//   document.querySelectorAll('.flecha-abajo').forEach(toggleInput);
+// };
+//   var aside = document.querySelector('.preview');
+//   toggleInput(aside);
+//   var main = document.querySelector('main');
+// toggleInput(main);
+
+// window.onload = function (){
+//   document.querySelectorAll('.eye-logo').forEach(function (input) {
+//     input.addEventListener('click', toggle);
+//
+//   });
+// };
