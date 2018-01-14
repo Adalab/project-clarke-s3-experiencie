7e6e721a0c64864b208469e5889b0f2ec605971d
Adalaber <mvtercero85@gmail.com>
2018-01-14 14:31:25 +0100

    Add jsmenu lateral and button close with animation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+var hamburguerButton= document.querySelector('.hamburguer-button');
+var menuLateral= document.querySelector('.menu-lateral');
+
+function showMenu() {
+  menuLateral.classList.toggle('animation-menu');
+  hamburguerButton.classList.toggle('close');
+}
+
+hamburguerButton.addEventListener('click', showMenu);
