12e69ea3e9c79944ac50ed6ecc577e15c9cfdd87
Adalaber <mvtercero85@gmail.com>
2018-01-15 16:16:56 +0100

    Deletebootstrap

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@
 var hamburguerButton= document.querySelector('.hamburguer-button');
 var hamburguerButtonClose= document.querySelector('.hamburguer-button-close');
 var menuLateral= document.querySelector('.menu-lateral');
-var burguerDown= document.querySelector('.burguer-down');
+// var burguerDown= document.querySelector('.burguer-down');
 
 function showMenu() {
   menuLateral.classList.toggle('animation-menu');
   hamburguerButton.classList.toggle('close-button');
-  burguerDown.classList.toggle('animation-burguer');
+  // burguerDown.classList.toggle('animation-burguer');
 }
 
 hamburguerButton.addEventListener('click', showMenu);
