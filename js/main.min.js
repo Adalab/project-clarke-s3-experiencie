e4856dde1828d4280503a6b6d7b34bdffccdb414
Adalaber <mvtercero85@gmail.com>
2018-01-15 18:21:31 +0100

    Clean code

--- js/main.min.js
+++ js/main.min.js
@@ -3,13 +3,10 @@
 var hamburguerButton= document.querySelector('.hamburguer-button');
 var hamburguerButtonClose= document.querySelector('.hamburguer-button-close');
 var menuLateral= document.querySelector('.menu-lateral');
-// var burguerDown= document.querySelector('.burguer-down');
 
 function showMenu() {
   menuLateral.classList.toggle('animation-menu');
   hamburguerButton.classList.toggle('close-button');
-
-  // burguerDown.classList.toggle('animation-burguer');
 }
 
 hamburguerButton.addEventListener('click', showMenu);
